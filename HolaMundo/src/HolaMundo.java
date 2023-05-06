4126b02c5590b08418bd6f6742237f22ad79ae64
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-05-06 11:09:38 -0500

    Se imprimira mi nombre

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args) throws Exception {
         System.out.println("¡Hola mundo!");
         System.out.println("Biendevida al curso");
+        System.out.println("Gonzalo");
     }
 }
